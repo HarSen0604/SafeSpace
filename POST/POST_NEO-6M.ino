d9c1d35a89633403883dd630ab57bdfcb4b71b89
S Karun Vikhash <137052983+TGravitas@users.noreply.github.com>
2024-02-19 00:14:13 +0530

    Update POST_NEO-6M.ino
    Rough Code implemented

--- POST/POST_NEO-6M.ino
+++ POST/POST_NEO-6M.ino
@@ -1,36 +1,123 @@
-#include <SoftwareSerial.h> // Include the SoftwareSerial library for serial communication
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
-// Define the pins for serial communication with the NEO-6M module
-#define NEO_6M_RX_PIN 2 // Connect NEO-6M TX pin to Arduino digital pin 2
-#define NEO_6M_TX_PIN 3 // Connect NEO-6M RX pin to Arduino digital pin 3
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
 
-SoftwareSerial neoSerial(NEO_6M_RX_PIN, NEO_6M_TX_PIN); // Create a SoftwareSerial object
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
-  Serial.begin(9600); // Initialize serial communication with the computer
-  neoSerial.begin(9600); // Initialize serial communication with the NEO-6M module
-  delay(1000); // Wait for the NEO-6M module to initialize
-  performPOST(); // Perform Power-On Self Test
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
 }
 
 void loop() {
-  // Nothing to do here in this example
-}
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      // Latitude in degrees (double)
+      Serial.print("Latitude= ");
+      Serial.print(gps.location.lat(), 6);
+      // Longitude in degrees (double)
+      Serial.print(" Longitude= ");
+      Serial.println(gps.location.lng(), 6);
 
-void performPOST() {
-  // Print a message indicating the start of the POST
-  Serial.println("Starting Power-On Self Test (POST) for NEO-6M GPS module...");
+      // Raw latitude in whole degrees
+      Serial.print("Raw latitude = ");
+      Serial.print(gps.location.rawLat().negative ? "-" : "+");
+      Serial.println(gps.location.rawLat().deg);
+      // ... and billionths (u16/u32)
+      Serial.println(gps.location.rawLat().billionths);
 
-  // Check if NEO-6M module is responding
-  neoSerial.println("$PMTK000*32");
-  delay(100); // Wait for response
-  if (neoSerial.find("$PMTK001,0,3,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")) {
-    // NEO-6M module is responding correctly
-    Serial.println("NEO-6M GPS module is responding.");
-    Serial.println("Power-On Self Test (POST) passed.");
-  } else {
-    // NEO-6M module is not responding correctly
-    Serial.println("NEO-6M GPS module is not responding.");
-    Serial.println("Power-On Self Test (POST) failed.");
+      // Raw longitude in whole degrees
+      Serial.print("Raw longitude = ");
+      Serial.print(gps.location.rawLng().negative ? "-" : "+");
+      Serial.println(gps.location.rawLng().deg);
+      // ... and billionths (u16/u32)
+      Serial.println(gps.location.rawLng().billionths);
+
+      // Raw date in DDMMYY format (u32)
+      Serial.print("Raw date DDMMYY = ");
+      Serial.println(gps.date.value());
+
+      // Year (2000+) (u16)
+      Serial.print("Year = ");
+      Serial.println(gps.date.year());
+      // Month (1-12) (u8)
+      Serial.print("Month = ");
+      Serial.println(gps.date.month());
+      // Day (1-31) (u8)
+      Serial.print("Day = ");
+      Serial.println(gps.date.day());
+
+      // Raw time in HHMMSSCC format (u32)
+      Serial.print("Raw time in HHMMSSCC = ");
+      Serial.println(gps.time.value());
+
+      // Hour (0-23) (u8)
+      Serial.print("Hour = ");
+      Serial.println(gps.time.hour());
+      // Minute (0-59) (u8)
+      Serial.print("Minute = ");
+      Serial.println(gps.time.minute());
+      // Second (0-59) (u8)
+      Serial.print("Second = ");
+      Serial.println(gps.time.second());
+      // 100ths of a second (0-99) (u8)
+      Serial.print("Centisecond = ");
+      Serial.println(gps.time.centisecond());
+
+      // Raw speed in 100ths of a knot (i32)
+      Serial.print("Raw speed in 100ths/knot = ");
+      Serial.println(gps.speed.value());
+      // Speed in knots (double)
+      Serial.print("Speed in knots/h = ");
+      Serial.println(gps.speed.knots());
+      // Speed in miles per hour (double)
+      Serial.print("Speed in miles/h = ");
+      Serial.println(gps.speed.mph());
+      // Speed in meters per second (double)
+      Serial.print("Speed in m/s = ");
+      Serial.println(gps.speed.mps());
+      // Speed in kilometers per hour (double)
+      Serial.print("Speed in km/h = ");
+      Serial.println(gps.speed.kmph());
+
+      // Raw course in 100ths of a degree (i32)
+      Serial.print("Raw course in degrees = ");
+      Serial.println(gps.course.value());
+      // Course in degrees (double)
+      Serial.print("Course in degrees = ");
+      Serial.println(gps.course.deg());
+
+      // Raw altitude in centimeters (i32)
+      Serial.print("Raw altitude in centimeters = ");
+      Serial.println(gps.altitude.value());
+      // Altitude in meters (double)
+      Serial.print("Altitude in meters = ");
+      Serial.println(gps.altitude.meters());
+      // Altitude in miles (double)
+      Serial.print("Altitude in miles = ");
+      Serial.println(gps.altitude.miles());
+      // Altitude in kilometers (double)
+      Serial.print("Altitude in kilometers = ");
+      Serial.println(gps.altitude.kilometers());
+      // Altitude in feet (double)
+      Serial.print("Altitude in feet = ");
+      Serial.println(gps.altitude.feet());
+
+      // Number of satellites in use (u32)
+      Serial.print("Number os satellites in use = ");
+      Serial.println(gps.satellites.value());
+
+      // Horizontal Dim. of Precision (100ths-i32)
+      Serial.print("HDOP = ");
+      Serial.println(gps.hdop.value());
+    }
   }
 }
